87611984b41acc20ffe68ced9a5c6cd03d184702
Johan West <johan@ispy.se>
2015-08-18 22:38:45 +0200

    update array split to use lodash's chunk fn

--- src/array/split.js
+++ src/array/split.js
@@ -1,6 +1,5 @@
 import inv from 'invariant';
-import drop from 'lodash/array/drop';
-import take from 'lodash/array/take';
+import chunk from 'lodash/array/chunk';
 import isArr from 'lodash/lang/isArray';
 import isNum from 'lodash/lang/isNumber';
 
@@ -15,35 +14,5 @@ export default function split(arr, n) {
   inv(isArr(arr), 'Expecting `arr` to be an array');
   inv(isNum(n) && n > 1, 'Expecting `n` to be a positive number (> 1)');
 
-  let subArrs = timesDividableBy(arr.length, n);
-  let result = [];
-
-  while(subArrs) {
-    result = [...result, take(arr, n)];
-    arr = drop(arr, n);
-
-    subArrs--;
-  }
-
-  return result;
-}
-
-/**
- * Calculate how many times `len` is dividable by `n`.
- *
- * @param  {number} len
- * @param  {number} n
- * @return {number}
- */
-function timesDividableBy(len, n) {
-  if(len === 0) return 0;
-
-  let count = 1;
-
-  while(len > n) {
-    count++;
-    len -= n;
-  }
-
-  return count;
-}
+  return chunk(arr, n);
+};
